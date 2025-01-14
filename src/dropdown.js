7707cd908e83746942cd5d24c1f79098ac6a9e7f
danielz0102 <danyzglez@hotmail.com>
2025-01-13 19:15:49 -0600

    Create dropdown module

---
+++ src/dropdown.js
@@ -0,0 +1,45 @@
+import '@/dropdown.css'
+
+const dropdown = document.querySelector('.dropdown')
+const btn = document.querySelector('.dropdown__btn')
+const list = document.querySelector('.dropdown__list')
+
+const ERROR_MESSAGES = {
+  dropdown: 'The element .dropdown was not found',
+  btn: 'The element .dropdown__btn was not found',
+  list: 'The element .dropdown__list was not found',
+  trigger:
+    "The trigger type selected is unknown. Please select 'click' or 'hover'.",
+}
+
+const elements = { dropdown, btn, list }
+
+for (const [key, element] of Object.entries(elements)) {
+  if (!element) {
+    console.error(`Dropdown error: ${ERROR_MESSAGES[key]}`)
+  }
+}
+
+try {
+  const triggerType = btn.dataset.trigger
+  if (triggerType === 'click') {
+    btn.addEventListener('click', (e) => {
+      list.classList.toggle('hidden')
+    })
+  } else if (triggerType === 'hover') {
+    btn.addEventListener('mouseenter', (e) => {
+      list.classList.remove('hidden')
+    })
+
+    dropdown.addEventListener('mouseleave', (e) => {
+      list.classList.add('hidden')
+    })
+  } else {
+    const id = dropdown.id || 'No id found'
+    console.error(
+      `Dropdown error: ${ERROR_MESSAGES.trigger}\n\nDropdown ID: ${id}`,
+    )
+  }
+} catch (error) {
+  console.error(error)
+}
